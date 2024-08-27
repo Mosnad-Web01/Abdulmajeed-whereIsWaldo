856eda745b4689f78ce72efc06436329b67c3f9e
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 14:57:29 +0300

    add Return the coordinates ([row, col]) of the element that differs from the rest

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ const example = [
 
 
 const whereIsWaldo = (matrix) => {
+  const rowLength = matrix.length;
+  const colLength = matrix[0].length;
+  const row = [];
+  const col = [];
+  const waldo = [];
+  for (let i = 0; i < rowLength; i++) {
+    for (let j = 0; j < colLength; j++) {
+      if (matrix[i][j] === "A") {
+        row.push(i);
+        col.push(j);
+      }
+    }
+  }
+  for (let i = 0; i < rowLength; i++) {
+    for (let j = 0; j < colLength; j++) {
+      if (matrix[i][j] === "B") {
+        waldo.push(i+1);
+        waldo.push(j+1);
+      }  
+    }
+  }
+  return waldo;
+  
   
 }
 
